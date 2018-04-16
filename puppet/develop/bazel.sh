aeba809e60091f11340b188903dc983f1075bf9c
zibo <gzb0506@gmail.com>
2018-04-15 19:34:13 -0700

    2nd

--- puppet/develop/bazel.sh
+++ puppet/develop/bazel.sh
@@ -1,2 +1,3 @@
 echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
 curl https://bazel.build/bazel-release.pub.gpg | apt-key add -
+apt-get update
